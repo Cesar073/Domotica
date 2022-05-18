66373e138e1ab45c8ab31b9595169c609fda89d3
Cesar <cesarbaby@gmail.com>
2022-05-18 14:49:06 -0300

    Enviando primer bucle

--- Pgm-Rpi-Arduino/handler.py
+++ Pgm-Rpi-Arduino/handler.py
@@ -1,18 +1,40 @@
 import serial
 import time
+import threading
 
 dev = serial.Serial("/dev/ttyUSB0", 9600)
-cadena = "CGVT005T005"
+
 #dev.write(cadena.encode('ascii').rstrip())
 # print(f"cadena:{str(cadena.encode('ascii').rstrip())}")
 
-counter = 0
-while True:
-    dev.write(cadena.encode('ascii'))
-    datos = dev.readline()
-    counter += 1
-    if counter == 1000:
-        counter = 0
-    print(datos.decode('utf-8') + " - " + str(counter))
-    time.sleep(0.5)
+def bucle_de_escritura():
+    while True:
+        opcion = input("""
+        1 - Consultar datos generales
+        2 - Ver sensor luz
+        3 - Ver sensor temp
+        elija:
+        """)
+        if opcion == "1":
+            cadena = "CGVFF050F050"
+            dev.write(cadena.encode('ascii'))
+        elif opcion == "2":
+            cadena = "CGVTF050F050"
+            dev.write(cadena.encode('ascii'))
+        elif opcion == "3":
+            cadena = "CGVFT050F050"
+            dev.write(cadena.encode('ascii'))
+        else:
+            print("Error de opción")
+
+
+def bucle_de_lectura():
+    while True:
+        datos = dev.readline()
+        print(datos.decode('utf-8'))
+        time.sleep(0.1)
 #dev.close()
+
+
+t1 = threading.Thread(name="hilo_1", target=bucle_de_escritura).start()
+t2 = threading.Thread(name="hilo_2", target=bucle_de_lectura).start()
